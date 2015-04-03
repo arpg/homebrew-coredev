708f9ba6fde8077db90f172f8f3222d8c89e288a
Gabe Sibley <gsibley@gwu.edu>
2015-04-02 22:32:40 -0600

    adds sophus brew formulae

--- sophus.rb
+++ sophus.rb
@@ -2,17 +2,14 @@
 #                /usr/local/Library/Contributions/example-formula.rb
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
-class Node < Formula
-  homepage "https://github.com/arpg/Node"
+class Sophus < Formula
+  homepage "https://github.com/arpg/Sophus"
 #  url "https://github.com/arpg/Node/archive/node-2.0.tar.gz"
 #  sha1 "ea9b151ac9f0afd584a2680db77bd4828ef99357"
-  head "https://github.com/arpg/Node.git", :branch => "homebrew"
+  head "https://github.com/arpg/Sophus.git", :branch => "homebrew"
 
   depends_on "cmake" => :build
-  depends_on "protobuf"
-#  depends_on "zmqpp"
-  depends_on "gflags"
-  depends_on "glog"
+  depends_on "eigen"
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
